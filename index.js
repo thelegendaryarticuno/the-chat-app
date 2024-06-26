52f2dcbc7a0c33b74fbcc09053b6d1633094530b
Naman Mani <142525587+thelegendaryarticuno@users.noreply.github.com>
2024-06-26 21:41:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,17 +8,19 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  try{
-
-    const r = await axios.put('https://api.chatengine.io/users/',
-        {username: username,secret: username,first_name: username},
-        {headers:{"private-key":"a654944d-a083-424e-ba14-f8dd06187985"}}
-    )
-    return res.status(r.status).json(r.data)
-  } catch(e) {
-    return res.status(e.response.status).json(e.response.data)
+  try {
+    const r = await axios.put(
+      'https://api.chatengine.io/users/',
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "a654944d-a083-424e-ba14-f8dd06187985" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
   }
- 
 });
 
-app.listen(3001);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
